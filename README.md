6e2f2171b73340192e825dbab8260a0f08c76a8e
Peter Barnum <peterbarnum@gmail.com>
2025-12-01 23:34:42 -0700

    complete day 2 prompt 1 + 2

--- README.md
+++ README.md
@@ -27,7 +27,7 @@
     <img width='50px' height='auto' src='assets/badges/complete/day1.svg'/>
   </a>
   <a href='day02/README.md'>
-    <img width='50px' height='auto' src='assets/badges/incomplete/day2.svg'/>
+    <img width='50px' height='auto' src='assets/badges/complete/day2.svg'/>
   </a>
   <a href='day03/README.md'>
     <img width='50px' height='auto' src='assets/badges/incomplete/day3.svg'/>
--- day02/index.js
+++ day02/index.js
@@ -1,16 +1,149 @@
 const fs = require('fs/promises');
-   const path = require('path');
+const path = require('path');
 
-   /** @returns {Promise<string[]>} */
-   const getParsedData = async (file = 'data.txt') => {
-     const data = await fs.readFile(path.resolve(__dirname, file), 'utf8');
-     return data.split(/\r?\n/);
-   };
+/** @returns {Promise<{start: string, end: string }[]>} */
+const getParsedData = async (file = 'data.txt') => {
+  const data = await fs.readFile(path.resolve(__dirname, file), 'utf8');
+  return data
+    .replaceAll(/\r?\n/g, '')
+    .split(',')
+    .map(l => {
+      const [start, end] = l.split('-');
+      return { start, end };
+    });
+};
 
-   const day02 = async () => {
-     const lines = await getParsedData();
-     console.log(lines)
-   };
+/**
+ * Check if a sequence of numbers repeats twice in number string
+ * @param {string} str
+ *
+ * @example
+ * Since the young Elf was just doing silly patterns, you can find the invalid IDs by
+ * looking for any ID which is made only of some sequence of digits repeated twice.
+ * So, 55 (5 twice), 6464 (64 twice), and 123123 (123 twice) would all be invalid IDs.
+ */
+const checkRepeatTwice = (str) => {
+  const len = str.length;
+  // only check even length strings
+  if (len % 2 !== 0) {
+    return null;
+  }
+  const [a, b] = [str.slice(0, len / 2), str.slice(len / 2)];
 
-   module.exports = day02;
- 
+  if (a === b) {
+    return str;
+  }
+};
+
+/**
+ *
+ * @param {string} start
+ * @param {string} end
+ */
+const findInvalidIds = (start, end) => {
+  let total = 0;
+
+  for (let i = parseInt(start); i <= parseInt(end); i++) {
+    const s = String(i);
+
+    const invalidStr = checkRepeatTwice(s);
+    if (invalidStr) {
+      total += i;
+    }
+  }
+
+  return total;
+};
+
+const day02PartOne = async () => {
+  const lines = await getParsedData();
+  // const lines = await getParsedData('example.txt');
+
+  const data = lines.map(({ start, end }) => ({
+    start,
+    end,
+    total: findInvalidIds(start, end)
+  }));
+
+  const total = data.reduce((total, d) => total + d.total, 0).toString();
+
+  console.log(`Total: ${total}`, total === '29940924880'); // 29940924880
+};
+
+/**
+ * Check if a sequence of numbers repeats twice in number string
+ * @param {string} str
+ *
+ * @example
+ * Since the young Elf was just doing silly patterns, you can find the invalid IDs by
+ * looking for any ID which is made only of some sequence of digits repeated twice.
+ * So, 55 (5 twice), 6464 (64 twice), and 123123 (123 twice) would all be invalid IDs.
+ */
+const checkRepeatAny = str => {
+  const len = str.length;
+  // only check even length strings
+
+  const minLength = 1;
+  const maxLength = Math.floor(str.length / 2);
+
+  const repeats = [];
+  for (let i = minLength; i <= maxLength; i++) {
+    if (len % i !== 0) {
+      // not possible to repeat n times
+      continue;
+    }
+    const segments = new Array(len / i).fill(0).map((_, j) => {
+      const s = i * j;
+      const e = s + i;
+      return str.slice(s, e);
+    });
+
+    const [a, ...rest] = segments;
+
+    const match = rest.every(v => v === a);
+
+    if (match) {
+      return true;
+    }
+  }
+  return false;
+};
+
+/**
+ *
+ * @param {string} start
+ * @param {string} end
+ */
+const findAnyInvalidIds = (start, end) => {
+  let total = 0;
+
+  for (let s = parseInt(start); s <= parseInt(end); s++) {
+    const n = String(s);
+
+    const invalidStr = checkRepeatAny(n);
+    if (invalidStr) {
+      total += s;
+    }
+  }
+
+  return total;
+};
+
+const day02PartTwo = async () => {
+  const lines = await getParsedData();
+
+  const data = lines.map(({ start, end }) => {
+    return { start, end, total: findAnyInvalidIds(start, end) };
+  });
+
+  const total = data
+    .reduce((total, d) => {
+      return total + d.total;
+    }, 0)
+    .toString();
+
+  console.log(`Total: ${total}`, total === '48631958998'); // 48631958998
+};
+
+module.exports = day02PartOne;
+module.exports = day02PartTwo;
