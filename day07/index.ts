c5094dc0facc736d4ff80127191cc5ec65553310
Peter Barnum <peterbarnum@gmail.com>
2025-12-10 17:13:49 -0700

    cleanup day 7

--- day07/index.ts
+++ day07/index.ts
@@ -9,117 +9,79 @@ const getParsedData = async (file = 'data.txt'): Promise<string[][]> => {
   return data.split(/\r?\n/).map((r) => r.split('').map((v) => (v === '.' ? '.' : v)));
 };
 
-const traverseDown = (
+const createPaths = (
   grid: string[][],
   rowIndex: number,
   colIndexes: Set<number>,
-  allColIndexes: number[][] = [],
-): { grid: string[][]; allColIndexes: number[][] } => {
+  splits = 0,
+): { splits: number; grid: string[][] } => {
   const prev = grid[rowIndex - 1] as string[];
   const row = grid[rowIndex] as string[];
 
-  if (!row || !prev) {
-    return { grid, allColIndexes };
+  if (!row || !prev || rowIndex >= grid.length) {
+    return { splits, grid };
   }
 
-  if (rowIndex < grid.length) {
-    colIndexes.forEach((colIndex) => {
-      const ch = row[colIndex];
-      const pch = prev[colIndex];
+  colIndexes.forEach((colIndex) => {
+    const char = row[colIndex];
+    const prevChar = prev[colIndex];
 
-      if (rowIndex >= grid.length) {
-        return { lines: grid, allColIndexes };
-      }
+    if (char === '^' && prevChar === '|') {
+      colIndexes.delete(colIndex);
+      const left = colIndex - 1;
+      const right = colIndex + 1;
 
-      if (pch === 'S') {
-        row[colIndex] = '|';
-        colIndex++;
-      }
+      row[left] = row[right] = '|';
 
-      if (pch === '|') {
-        if (ch === '^') {
-          if (colIndex !== 0) {
-            colIndexes.delete(colIndex);
-            row[colIndex - 1] = '|';
-            colIndexes.add(colIndex - 1);
-          }
+      colIndexes.add(left);
+      colIndexes.add(right);
 
-          if (colIndex < row.length - 1) {
-            colIndexes.delete(colIndex);
-            row[colIndex + 1] = '|';
-            colIndexes.add(colIndex + 1);
-          }
-        } else {
-          row[colIndex] = '|';
-        }
-      }
-    });
+      return splits++;
+    }
+    row[colIndex] = '|';
+  });
 
-    allColIndexes.push(Array.from(colIndexes.values()));
-    return traverseDown(grid, rowIndex + 1, colIndexes, allColIndexes);
-  }
-
-  return { grid, allColIndexes };
+  return createPaths(grid, rowIndex + 1, colIndexes, splits);
 };
 
-const cachedLR = new Map<string, { left: number; right: number }>();
-
-const walkPathFrom = (grid: string[][], rowIndex: number, colIndex: number): number => {
+const walkPathFrom = (
+  grid: string[][],
+  rowIndex: number,
+  colIndex: number,
+  LRC = new Map<string, { left: number; right: number }>(),
+): number => {
   if (rowIndex >= grid.length) {
     return 1;
   }
-
   const ch = grid[rowIndex]?.[colIndex];
-
   const nextCh = grid[rowIndex + 1]?.[colIndex];
 
   if (ch === '|' && nextCh === '^') {
-    const clr = cachedLR.get(`${rowIndex},${colIndex}`);
+    const lr = LRC.get(`${rowIndex},${colIndex}`);
+    const nextRow = rowIndex + 1;
 
-    const left = clr?.left ?? walkPathFrom(grid, rowIndex + 1, colIndex - 1);
-    const right = clr?.right ?? walkPathFrom(grid, rowIndex + 1, colIndex + 1);
+    const left = lr?.left ?? walkPathFrom(grid, nextRow, colIndex - 1, LRC);
+    const right = lr?.right ?? walkPathFrom(grid, nextRow, colIndex + 1, LRC);
 
-    cachedLR.set(`${rowIndex},${colIndex}`, { left, right });
+    LRC.set(`${rowIndex},${colIndex}`, { left, right });
 
     return left + right;
   }
-  if (!nextCh) {
-    return 1;
-  }
 
-  return walkPathFrom(grid, rowIndex + 1, colIndex);
-};
-
-const countSplits = (rows: string[][]) => {
-  let splitCount = 0;
-  for (let i = 1; i < rows.length; i++) {
-    const row = rows[i] as string[];
-    const prev = rows[i - 1] as string[];
-    for (let j = 0; j < rows.length; j++) {
-      const ch = row[j];
-      const pch = prev[j];
-      if (ch === '^' && pch === '|') {
-        splitCount++;
-      }
-    }
-  }
-  return splitCount;
+  return walkPathFrom(grid, rowIndex + 1, colIndex, LRC);
 };
 
 export const day07PartOne = async () => {
   const lines = await getParsedData();
   const startIndex = lines[0]?.findIndex((v) => v === 'S') ?? 0;
-  traverseDown(lines, 1, new Set([startIndex]));
-  return countSplits(lines);
+  return createPaths(lines, 1, new Set([startIndex])).splits;
 };
 
 export const day07PartTwo = async () => {
   const lines = await getParsedData();
   const startIndex = lines[0]?.findIndex((v) => v === 'S') ?? 0;
-  const grid = traverseDown(lines, 1, new Set([startIndex])).grid;
-  const totalPaths = walkPathFrom(grid, 1, startIndex);
-
-  return totalPaths;
+  const { grid } = createPaths(lines, 1, new Set([startIndex]));
+  return walkPathFrom(grid, 1, startIndex);
 };
 
 const day07 = async () => {
